3447f9b12cfa97ea7432c5fecdaf7b46dee89180
MisterFrani <bizifrani@icloud.com>
2025-04-11 13:57:12 +0200

    chore: addd texte

--- front/src/app/faq/page.tsx
+++ front/src/app/faq/page.tsx
@@ -118,7 +118,8 @@ export default function FAQPage() {
       <div className="bg-[#16803C] text-white py-20">
         <div className="max-w-4xl mx-auto px-4 text-center">
           <h1 className="text-3xl md:text-4xl font-bold mb-4">
-            Questions fréquentes
+            Questions fréquentes <br />
+            sur le thé
           </h1>
           <p className="text-lg md:text-xl text-white/90">
             Retrouvez toutes les réponses à vos questions
